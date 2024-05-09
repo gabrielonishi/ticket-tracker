199878ba6870d8bfa5382eb7dc459b7099362318
gabriel.onishi <gabriel.onishi@outlook.com>
2024-05-09 19:15:10 -0300

    feat(buy_ticket): starts buy_ticket script

---
+++ buy-ticket.py
@@ -0,0 +1,48 @@
+# Selenium imports
+import datetime
+import random
+import re
+import time
+import typing
+
+# Beautiful Soup imports
+import bs4
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+import utils
+
+
+def greet() -> typing.Tuple[str, str, datetime.time]:
+    event_name: str = input("Qual o nome da festa desejada?")
+    event_date: str = input("Qual a data do evento? (dd/mm/aaaa)")
+
+    # Validando data
+    while not re.match(r"\d{2}/\d{2}/\d{4}", event_date):
+        event_date = input(
+            "Data inválida. Digite novamente usando o formato dd/mm/aaaa")
+
+    event_time: str = input("Qual o horário do evento? (hh:mm)")
+
+    # Validando horário
+    while not re.match(r"\d{2}:\d{2}", event_time):
+        event_time = input(
+            "Horário inválido. Digite novamente usando o formato hh:mm")
+
+    horas: int = int(event_time.split(":")[0])
+    minutos: int = int(event_time.split(":")[1])
+
+    event_time: datetime.time = datetime.time(horas, minutos)
+
+    return event_name, event_date, event_time
+
+
+def main() -> None:
+    event_name, event_date, event_time = greet()
